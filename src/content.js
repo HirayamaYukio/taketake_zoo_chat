93a198cec999a539f1e4bd181b968c32215a4c4e
HirayamaYukio <monoswamp@gmail.com>
2024-01-13 15:55:25 +0900

    Fix prototype

--- src/content.js
+++ src/content.js
@@ -1,22 +1,37 @@
-console.log("taketakeスプリクトの読み込み成功");
-// チャットリストの配列を生成
-//
-//
-//
-//
-//
-// CONST
-const const_noname = "ThisNameIsNotOnTheList";
+/*
+ メインファイル。
+ 全体の流れ:
+ 1. タイマーの設定
+   ブラウザの読み込みを待つためにワンショットタイマでディレイを掛ける
+   (ホントは読み込み完了イベントの検知で発火させたほうがいい)
+ 2. 初期チャットへの置換処理(initChangeChatName();のcall)
+    初期表示されたチャットへ名前変換の処理を行う(50行くらい)
+ 3. 定期的に実行する名前変換処理を登録する
+    チャットが更新される(正確にはdocument.getElementById('root')以下)と
+    最新から数個のチャットに対して処理をする
+ ! 拡張機能を止めるときはChromeの管理機能からして下さい。
+*/
 
+console.log("taketakeスプリクトの読み込み成功");
+
+/*
+ 共通変数
+*/
+const const_noname = "ThisNameIsNotOnTheList";
 // ユーザークラス格納配列
 let userClassList = [];
+// 先頭名初期読み込み
+var init_firstname_array = firstname_str.split(/,/);
 // 先頭名管理クラス初期化
 var firstnameClass = new FirstnameClass(init_firstname_array);
-// ファイルからテンプレートの読み込み
-//console.log(animal_args);
-//const const_animal_list =  JSON.parse(animal_json.random_name);
+// 動物名初期読み込み
+const animal_args = animal_str.split(/\n/);
 
 
+
+/*
+  監視設定
+*/
 //  監視登録
 var observer = new MutationObserver(function(){
 	// 定期チャット名変更
@@ -25,9 +40,6 @@ var observer = new MutationObserver(function(){
 
 // 監視要素
 const elem = document.getElementById('root');
-//const elem = document.querySelectorAll('.chat-line__message');
-console.log(`OUT PUT observe : ${elem}`);
-
 const config = { 
   attributes: false, // ノードの属性変化を監視するかどうか
   childList: true, // 	子ノードの変化も監視するかどうか
@@ -35,7 +47,9 @@ const config = {
   subtree: true, // 子孫ノードの変化も監視するかどうか
 };
 
-
+/*
+  タイマーのコールバック関数と宣言
+*/
 //タイマー登録で初回処理をした後に、定期状態監視を登録する
 const log = function(){
     // 初回の名前変更
@@ -50,7 +64,10 @@ const log = function(){
 setTimeout(log, 3000);
 
 
-/** チャット名置換関数 */
+/*
+  ここから共通関数
+*/
+/** 初期表示チャット名置換関数 */
 function initChangeChatName() {
     // チャット一覧を取得
     var elements_list = document.getElementsByClassName('chat-line__message');
@@ -59,6 +76,7 @@ function initChangeChatName() {
     Array.prototype.forEach.call(elements_list, (value, index) => {
       try{
           var display_name = value.getElementsByClassName('chat-author__display-name');
+          var display_name_str = display_name[0].textContent; // 名前の文字列だけを抽出
           // textContentが取得できない場合は非チャットとして処理をスキップ
           //console.log(`OUT PUT will replace: ${animal[0].outerHTML}`);
           //console.log(`OUT PUT will replace: ${animal[0].textContent}`);
@@ -67,19 +85,19 @@ function initChangeChatName() {
 	  // UserClassListを確認して名前を変更したことがあるか確認
 	  userClassList.forEach(instance => {
 		  // 動物の名前になっていたら処理を1ループスキップ
-		  if (instance.confAnimalName(display_name[0]) && !hit_flg) throw "This name processed";
+		  if (instance.confAnimalName(display_name_str) && !hit_flg) throw "This name processed";
 
 		  // リストにすでに名前あり
-		  if (instance.confDefaultName(display_name[0]) && !hit_flg){
+		  if (instance.confDefaultName(display_name_str) && !hit_flg){
 		      tmp_name = instance.getAnimalName();
 	              hit_flg = true;
 		  }
 	  });
 
 	  // リストに名前がなければ動物名生成とユーザー配列に新しく追加
-          if(hit_flg == false){
+          if(!hit_flg){
 		  tmp_name = createAnimalName();
-		  var userClass = new UserClass(display_name[0],tmp_name);
+		  var userClass = new UserClass(display_name_str,tmp_name);
 		  userClassList.push(userClass);
 	  } 
  
@@ -92,7 +110,8 @@ function initChangeChatName() {
               delete_target[0].textContent = "";
 	  }
       }catch (e){
-          console.log(`Into catch e-> : ${e}`);
+          // delete_targetがない人が大体なので、ここのlogが大量に出る
+          //console.log(`Into catch e-> : ${e}`);
 
       }
     });
@@ -103,17 +122,45 @@ function periodicChangeChatName() {
     var elements_list = document.getElementsByClassName('chat-line__message');
     var elements_size = elements_list.length;
     
-    // とりあえず定期実行は10回しか回さないようにする
-    for (let i = 0; i < 10; i++) {
+    // とりあえず定期実行は最大5回しか回さないようにする
+    for (let i = 0; i < 5; i++) {
       try{
 	  // 後ろから回したいから新しいindexを作成
 	  var index = elements_size -i ;
-          var animal = elements_list[index].getElementsByClassName('chat-author__display-name');
+          //var animal = elements_list[index].getElementsByClassName('chat-author__display-name');
+          var display_name = elements_list[index].getElementsByClassName('chat-author__display-name');
           // textContentが取得できない場合は非チャットとして処理をスキップ
           var tmp_name = "ustreamer-12345";
-          if (animal[0].textContent != tmp_name){
-              animal[0].textContent = tmp_name;
-          }
+	  var hit_flg = false;
+          var display_name_str = display_name[0].textContent;
+	  // UserClassListを確認して名前を変更したことがあるか確認
+	  userClassList.forEach(instance => {
+		  // 動物の名前になっていたら処理を1ループスキップ
+		  if (instance.confAnimalName(display_name_str) && !hit_flg) throw "This name processed";
+
+		  // リストにすでに名前あり
+		  if (instance.confDefaultName(display_name_str) && !hit_flg){
+		      tmp_name = instance.getAnimalName();
+	              hit_flg = true;
+		  }
+	  });
+
+	  // 配信者のチャットの場合はそれとわかるようにする
+	  if(display_name_str == "たけっちノームコア" && delete_target[0].textContent == " (usttakecchi9)"){
+	      // リストに名前がなければ動物名生成とユーザー配列に新しく追加
+		  tmp_name = "でもでもでもでも👑たけっちさん";
+
+	  } 
+	  // リストに名前がなければ動物名生成とユーザー配列に新しく追加
+          else if(!hit_flg){
+	      // リストに名前がなければ動物名生成とユーザー配列に新しく追加
+		  tmp_name = createAnimalName();
+		  var userClass = new UserClass(display_name_str,tmp_name);
+		  userClassList.push(userClass);
+	  } 
+ 
+	 // チャット欄の名前を置き換える
+         display_name[0].textContent = tmp_name;
 
 	  // IDがある場合は削除
           var delete_target = elements_list[index].getElementsByClassName('chat-author__intl-login');
@@ -125,15 +172,13 @@ function periodicChangeChatName() {
 
       }
      }
-    //});
 }
 
 /** 新しい動物の名前を生成する関数 */
 // arg : なし
 // return : 動物名 (string)
 function createAnimalName() {
-        //console.log(`length -> : ${animal_args.length}`);
-	var result = "ustreamer-12345";
+	var result = "ustreamer-23456";
 
 	try{
   	    // 絵文字をランダムな数字で持ってくる
@@ -166,4 +211,3 @@ function createAnimalName() {
 	}
 
 }
-
